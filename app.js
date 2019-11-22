5e6f5e78163796f120791f7dabba81bfd4e5ac3c
노경모 <kyeongmo2@gmail.com>
2019-11-22 23:01:10 +0900

    Add error handling code to promise codes and async await codes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const recipeList = document.querySelector('.recipeList')
 
 let previousInputValue = ''
 
-const debouncedInputHandler = debounce(({ target: { value: inputValue } }) => {
+const debouncedInputHandler = debounce(async ({ target: { value: inputValue } }) => {
   if (!inputValue) {
     return
   }
@@ -20,16 +20,33 @@ const debouncedInputHandler = debounce(({ target: { value: inputValue } }) => {
 
   recipeList.innerHTML = '<h3>Loading...</h3>'
 
-  fetch(`${apiEndPoint}?f=${inputValue}`, {
-    method: 'GET',
-  })
-  .then((response) => response.json())
-  .then(({ meals }) => {
+  // fetch(`${apiEndPoint}?f=${inputValue}`, {
+  //   method: 'GET',
+  // })
+  // .then((response) => response.json())
+  // .then(({ meals }) => {
+  //   recipeList.innerHTML = meals
+  //     .map((meal) => new Meal(meal))
+  //     .map((mealInstance) => mealInstance.renderToString())
+  //     .join('')
+  // })
+  // .catch(() => {
+  //   recipeList.innerHTML = '<h3>An error has occurred when fetching data...</h3>'
+  // })
+
+  try {
+    const response = await fetch(`${apiEndPoint}?f=${inputValue}`, {
+      method: 'GET',
+    })
+    const { meals } = await response.json()
+
     recipeList.innerHTML = meals
       .map((meal) => new Meal(meal))
       .map((mealInstance) => mealInstance.renderToString())
       .join('')
-  })
+  } catch {
+    recipeList.innerHTML = '<h3>An error has occurred when fetching data...</h3>'
+  }
 
   previousInputValue = inputValue
 }, 1000)
